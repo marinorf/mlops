13139fcc975811057e00e4b9d27dcba5008c3a6d
marinorf <100943953+marinorf@users.noreply.github.com>
2025-05-04 18:27:51 +0900

    Removed unused packages + cleaned up comments

--- evaluate/evaluate_MLJar.py
+++ evaluate/evaluate_MLJar.py
@@ -1,23 +1,8 @@
 import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-
 from sklearn.metrics import confusion_matrix
-import seaborn as sns
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-
 from sklearn.metrics import classification_report
 import os
-import cv2
-
 from supervised.automl import AutoML
 
 # Load the preprocessed data
@@ -45,17 +30,16 @@ predictions_df.to_csv('/Users/marinorfolk/Desktop/MLops_rep/mlops/data/MLJar_pre
 
 # Load the leaderboard
 RESULTS_PATH = "models/AutoML_results"
-lb_csv = os.path.join(RESULTS_PATH, "leaderboard.csv")
-
 NUMBER_OF_MODELS = 3
+
+lb_csv = os.path.join(RESULTS_PATH, "leaderboard.csv")
 df = pd.read_csv(lb_csv)
 
 df_sorted = df.sort_values("metric_value", ascending=False)
 
-# Top models
 top = df_sorted.head(NUMBER_OF_MODELS)
 
 print(f"Top {NUMBER_OF_MODELS} models by validation score:")
-for i, row in top.iterrows():
-    print(f" {i+1}. {row['name']:20s} ↳ score = {row['metric_value']:.4f}")
+for i, model in top.iterrows():
+    print(f" {i}. {model['name']:20s} with score: {model['metric_value']:.4f}")
     
